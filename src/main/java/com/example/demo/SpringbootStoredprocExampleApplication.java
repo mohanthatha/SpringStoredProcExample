f8c29d9eac5a295d7aafd2ddee062b906599c875
mohanthatha <mohanchowdary79@gmail.com>
2022-09-02 18:57:39 +0530

    firstcommit

---
+++ src/main/java/com/example/demo/SpringbootStoredprocExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStoredprocExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStoredprocExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Employee;
+
+
+@RestController
+public class EmployeeController {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/emp-dtls")
+	public ResponseEntity<List<Employee>> getEmp(@RequestParam(name = "name") String name) {
+		List<Employee> employees = entityManager.createNamedStoredProcedureQuery("get_empoyee_details")
+				.setParameter("emp_name", name).getResultList();
+		return new ResponseEntity<>(employees, HttpStatus.OK);
+	}
+}
